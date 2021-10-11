b65a56c2f35600e1603aac3b11aaadf672e3fee8
hpazk <hpazk.zkh@gmail.com>
2021-10-11 14:02:19 +0700

    test: create.php 4

--- create.php
+++ create.php
@@ -16,7 +16,7 @@ if ($picture_file != "") {
   if (in_array($extension, $allowed) === true) {
     move_uploaded_file($file_tmp, 'images/' . $filename);
 
-    $query = "INSERT INTO m_pemutusan (IDPEL, description, FOTO) VALUES ('$title', '$description', '$filename')";
+    $query = "INSERT INTO m_pemutusan (IDPEL, FOTO) VALUES ('$title', '$filename')";
     $result = mysqli_query($conn, $query);
 
     if (!$result) {
@@ -31,7 +31,7 @@ if ($picture_file != "") {
     echo "invalid extension";
   }
 } else {
-  $query = "INSERT INTO m_pemutusan (title, description, picture_file) VALUES ('$title', '$description', null)";
+  $query = "INSERT INTO m_pemutusan (IDPEL, picture_file) VALUES ('$title', null)";
   $result = mysqli_query($conn, $query);
 
   if (!$result) {
