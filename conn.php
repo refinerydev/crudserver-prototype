9c6754ffc9f976e647a2440698df0f69e22624e8
hpazk <hpazk.zkh@gmail.com>
2021-10-11 11:20:58 +0700

    fix: conn

--- conn.php
+++ conn.php
@@ -7,17 +7,6 @@ $conn = mysqli_connect($host, $user, $pass, $dbname); //pastikan urutan nya sepe
 
 if (!$conn) { //jika tidak terkoneksi maka akan tampil error
   die(mysqli_connect_error());
-} else {
-  $query = mysqli_query($conn, 'SELECT * FROM m_pemutusan_wo');
-  while ($row = mysqli_fetch_assoc($query)) {
-    $result[] = $row;
-  }
-
-  if (!$result) {
-    die(mysqli_errno($conn));
-  }
-
-  echo $result;
 }
   
 /*CREATE TABLE `produk` ( `id` INT(11) NOT NULL AUTO_INCREMENT ,  `nama_produk` VARCHAR(255) NULL ,  `deskripsi` TEXT NULL ,  `harga_beli` INT(11) NULL ,  `harga_jual` INT(11) NULL ,  `gambar_produk` VARCHAR(255) NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;*/
