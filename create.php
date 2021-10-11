753d3b1c3bef36c10d96f8d2e1f4d7b827d4c49a
hpazk <hpazk.zkh@gmail.com>
2021-10-11 13:54:55 +0700

    test: create.php 2

--- create.php
+++ create.php
@@ -2,22 +2,22 @@
 include 'conn.php';
 header('Access-Control-Allow-Origin: *');
 
-$title   = $_POST[''];
+$title   = $_POST['title'];
 $description     = $_POST['description'];
-$picture_file = $_FILES['FOTO']['name'];
+$picture_file = $_FILES['picture_file']['name'];
 
 if ($picture_file != "") {
   $allowed = array('png', 'jpg');
   $x = explode('.', $picture_file);
   $extension = strtolower(end($x));
-  $file_tmp = $_FILES['FOTO']['tmp_name'];
+  $file_tmp = $_FILES['picture_file']['tmp_name'];
   $random     = rand(1, 999);
   $filename = $random . '-' . $picture_file;
 
   if (in_array($extension, $allowed) === true) {
     move_uploaded_file($file_tmp, 'images/' . $filename);
 
-    $query = "INSERT INTO m_pemutusan (FOTO) VALUES ('$filename')";
+    $query = "INSERT INTO notes (IDPEL, description, FOTO) VALUES ('$title', '$description', '$filename')";
     $result = mysqli_query($conn, $query);
 
     if (!$result) {
@@ -32,7 +32,7 @@ if ($picture_file != "") {
     echo "invalid extension";
   }
 } else {
-  $query = "INSERT INTO m_pemutusan (FOTO) VALUES (null)";
+  $query = "INSERT INTO notes (title, description, picture_file) VALUES ('$title', '$description', null)";
   $result = mysqli_query($conn, $query);
 
   if (!$result) {
