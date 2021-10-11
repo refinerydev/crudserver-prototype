36bbc84bc79afa334b7952b7d8fe0b04ae96e8d8
hpazk <hpazk.zkh@gmail.com>
2021-10-11 13:21:51 +0700

    update: readall.php

--- readall.php
+++ readall.php
@@ -4,20 +4,25 @@ include 'conn.php';
 header('Access-Control-Allow-Origin:*');
 header('Content-type: application/json');
 
-// $cat = $_GET['category'];
-// $page = $_GET['page'];
 
-
-
-$query = mysqli_query($conn, 'SELECT * FROM m_pemutusan_wo');
-while ($row = mysqli_fetch_assoc($query)) {
-  $result[] = $row;
+// category
+$category = mysqli_query($conn, 'SELECT DISTINCT KDDK FROM m_pemutusan_wo');
+while ($row_cat = mysqli_fetch_assoc($category)) {
+  $cat[] = $row_cat;
 }
 
-if (!$result) {
-  die(mysqli_errno($conn));
+
+function getData($c, $cat)
+{
+  $query = mysqli_query($c, 'SELECT * FROM m_pemutusan_wo WHERE KDDK = "' . $cat . '"');
+  while ($row = mysqli_fetch_assoc($query)) {
+    $result[] = $row;
+  }
+  return $result;
 }
 
-echo json_encode($result);
+for ($i = 0; $i < count($cat); $i++) {
+  $cat[$i]['data'] = getData($conn, $cat[$i]['KDDK']);
+}
 
-// echo json_encode($result);
+echo json_encode($cat);
